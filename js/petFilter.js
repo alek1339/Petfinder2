c2e0b0dda456f71a4537988d27b63e7bcc1fde95
Aleksandar <aleksandermihaylov@gmail.com>
2019-03-06 11:10:38 +0200

    filter aded and some media changes

--- js/petFilter.js
+++ js/petFilter.js
@@ -1,28 +1,48 @@
-$('#genderOptions').change(function(e) {
-    if(document.getElementById('pets') != 'undefined'){
-        document.getElementById('pets').innerHTML = ''
+$('#genderOptions').change(function (e) {
+  petFilter()
+})
+
+$('#yearsOptions').change(function (e) {
+  petFilter()
+})
+
+function petFilter () {
+  if (document.getElementById('pets') != 'undefined') {
+    document.getElementById('pets').innerHTML = ''
+  }
+
+  (async () => {
+    let adopt = await $.get('../templates/adopt.hbs')
+    let template = Handlebars.compile(adopt)
+
+    $.ajax('http://localhost:5000/animals/getAnimals')
+      .then(response => addAllAnimals(response))
+
+    function addAllAnimals (allPets) {
+      var animals = []
+      if ($('#genderOptions').val() === 'male' && $('#yearsOptions').val() !== 'all') {
+        animals = _.filter(allPets, { 'gender': 'Male', 'years': $('#yearsOptions').val() })
+        console.log(1)
+      } else if ($('#genderOptions').val() === 'female' && $('#yearsOptions').val() !== 'all') {
+        animals = _.filter(allPets, { 'gender': 'Female', 'years': $('#yearsOptions').val() })
+        console.log(2)
+      } else if ($('#genderOptions').val() === 'all' && $('#yearsOptions').val() !== 'all') {
+        animals = _.filter(allPets, { 'years': $('#yearsOptions').val() })
+        console.log(3)
+      } else if ($('#genderOptions').val() === 'male' && $('#yearsOptions').val() === 'all') {
+        animals = _.filter(allPets, { 'gender': 'Male' })
+        console.log(5)
+      } else if ($('#genderOptions').val() === 'female' && $('#yearsOptions').val() === 'all') {
+        animals = _.filter(allPets, { 'gender': 'Female' })
+      } else if (($('#genderOptions').val() === 'all' && $('#yearsOptions').val() === 'all')) {
+        animals = allPets
+        console.log(6)
+      }
+
+      for (const pet of animals) {
+        let resultHtml = template(pet)
+        $('#pets').append(Handlebars.compile(resultHtml))
+      }
     }
-
-        // var gender = $( "#genderOptions" ).val()
-        // console.log(gender)
-
-    (async () => {
-        let adopt = await $.get('../templates/adopt.hbs')
-        let template = Handlebars.compile(adopt)
-
-        // let animals = {}
-
-        $.ajax('http://localhost:5000/animals/getAnimals')
-            .then(response => addAllAnimals(response))
-
-
-        function addAllAnimals(animals) {
-            console.log(animals)
-            for (const pet of animals) {
-                let resultHtml = template(pet)
-                $("#pets").append(Handlebars.compile(resultHtml))
-            }
-        }
-
-    })()
-});
+  })()
+}
